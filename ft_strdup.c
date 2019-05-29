691e7cd6e3ff8a6b4047559d4167120667d4f35b
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-05-29 09:20:17 +0200

    new

---
+++ ft_strdup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/28 09:40:32 by nlunga            #+#    #+#             */
+/*   Updated: 2019/05/28 10:16:54 by nlunga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char *new;
+
+	new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if (new == NULL)
+		return (NULL);
+	ft_strcpy(new, str);
+	return (new);
+}
