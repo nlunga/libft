14da311d24cce9a7362aa862abde4670966203cf
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-06-03 16:47:13 +0200

    Modified

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:30:23 by nlunga            #+#    #+#             */
-/*   Updated: 2019/05/28 09:37:46 by nlunga           ###   ########.fr       */
+/*   Updated: 2019/06/03 15:28:30 by nlunga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ char	*ft_strncpy(char *dest, const char *src, size_t len)
 	size_t i;
 
 	i = 0;
-	while (i < len && src[i])
+	while (i < len && src[i] != '\0' && dest[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (i < len)
+	while (i < len && dest[i] != '\0')
+	{
 		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
