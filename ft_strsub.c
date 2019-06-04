91224d4502f9320405fc8aa6c5790723cd213cc3
Nqabutho LUNGA <nlunga@c5r11s8.wethinkcode.co.za>
2019-06-04 16:43:05 +0200

    strsub

---
+++ ft_strsub.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 14:58:03 by nlunga            #+#    #+#             */
+/*   Updated: 2019/06/04 16:20:38 by nlunga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char		*new;
+	size_t		i;
+
+	if (!s || !(new = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		new[i] = s[start + i];
+	new[i] = '\0';
+	return (new);
+}
