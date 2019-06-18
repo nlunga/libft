a38ac5751f69601c7839278d5fbf8d1fc85bce3a
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-06-18 11:18:57 +0200

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 08:38:23 by nlunga            #+#    #+#             */
-/*   Updated: 2019/06/13 12:52:27 by nlunga           ###   ########.fr       */
+/*   Updated: 2019/06/18 09:05:13 by nlunga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t				ft_strlen(const char *str);
+int					ft_intlen(int n);
 char				*ft_strdup(const char *str);
 char				*ft_strcpy(char *dest, const char *src);
 char				*ft_strncpy(char *dest, const char *src, size_t len);
